033d0d55575c6c47a69b7b1f622a645bea627b7f
Dan1Gimenez <dani.gimenez5330@gmail.com>
2024-09-11 23:16:14 -0300

    s

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ItemListContainer = () => {
+    return (
+        <section id="itemListContainer">
+            <h2>Bienvenido a nuestra tienda</h2>
+            <p>Explora nuestros productos haciendo clic en las categorías de arriba.</p>
+        </section>
+    );
+};
+
+export default ItemListContainer;
